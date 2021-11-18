ea542571d49e0f4155bae7d442c704a01f079123
Seyedehgolbarg Hossininezhad Khorasgani <seyedehgolbarg.hossininezhad-khorasgani@etu.u-pec.fr>
2021-11-18 19:20:54 +0330

    product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { Header } from './components/header/Header';
+import { Products } from './components/products/Products';
 
 function App() {
+  const products = [
+    {
+      id: 'p1',
+      name: 'Toilet Paper',
+      price: 94.12,
+    },
+    { id: 'p2', name: 'TV', price: 799.49},
+    {
+      id: 'p3',
+      name: 'laptop',
+      price: 294.67
+    },
+    {
+      id: 'p4',
+      name: 'Desk',
+      price: 450,
+    },
+  ];
   return (
     <div >
      <Header />
+     <Products products={products}/>
     </div>
   );
 }
---
+++ src/components/product/Product.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+export const Product = ({productName , price , img}) => {
+  return (
+    <div className="cardContent">
+      {/* <img src={img} className="cardImg" alt="imageCard" /> */}
+      <div className="cardInfo">
+        <p className="cardPart">
+          <span className="cardTitle">Product Name : </span> {productName}
+        </p>
+        <p className="cardPart">
+          <span className="cardTitle">Price :</span> {price}
+        </p>
+      </div>
+    </div>
+  )
+}
