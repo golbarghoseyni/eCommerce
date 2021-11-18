ab7b420d298e94782ef31491ece6aef51dab5026
Seyedehgolbarg Hossininezhad Khorasgani <seyedehgolbarg.hossininezhad-khorasgani@etu.u-pec.fr>
2021-11-18 18:12:48 +0330

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import { Header } from './components/header/Header';
 
 function App() {
   return (
     <div >
-     <h1>welcome to react </h1>
+     <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="headerText">Welcome to ECommerce Website</h1>
+    </div>
+  )
+}
